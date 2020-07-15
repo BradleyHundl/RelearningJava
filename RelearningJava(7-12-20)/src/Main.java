343f85e0d1e84a9cdf8841fb2490ebdf5ad28167
Bradley Hundl <bradley.b.hundl-1@ou.edu>
2020-07-15 00:29:39 -0500

    fixing up to date

---
+++ RelearningJava(7-12-20)/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		String apples = "yummy";
+		
+		String there = "time";
+		
+		Scanner scan = new Scanner(there);
+		
+		
+		
+		
+		
+		
+	}
+
+}
